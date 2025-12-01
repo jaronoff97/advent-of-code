d7533413f6e79c01f0dd7d1c725718a1edb2916a
jaronoff97 <jacobaronoff45@gmail.com>
2025-12-01 14:16:53 -0500

    init'd

---
+++ build.zig
@@ -0,0 +1,85 @@
+const std = @import("std");
+
+// Although this function looks imperative, it does not perform the build
+// directly and instead it mutates the build graph (`b`) that will be then
+// executed by an external runner. The functions in `std.Build` implement a DSL
+// for defining build steps and express dependencies between them, allowing the
+// build runner to parallelize the build automatically (and the cache system to
+// know when a step doesn't need to be re-run).
+pub fn build(b: *std.Build) void {
+    // Standard target options allow the person running `zig build` to choose
+    // what target to build for. Here we do not override the defaults, which
+    // means any target is allowed, and the default is native. Other options
+    // for restricting supported target set are available.
+    const target = b.standardTargetOptions(.{});
+    // Standard optimization options allow the person running `zig build` to select
+    // between Debug, ReleaseSafe, ReleaseFast, and ReleaseSmall. Here we do not
+    // set a preferred release mode, allowing the user to decide how to optimize.
+    const optimize = b.standardOptimizeOption(.{});
+    // It's also possible to define more custom flags to toggle optional features
+    // of this build script using `b.option()`. All defined flags (including
+    // target and optimize options) will be listed when running `zig build --help`
+    // in this directory.
+
+    // This creates a module, which represents a collection of source files alongside
+    // some compilation options, such as optimization mode and linked system libraries.
+    // Zig modules are the preferred way of making Zig code available to consumers.
+    // addModule defines a module that we intend to make available for importing
+    // to our consumers. We must give it a name because a Zig package can expose
+    // multiple modules and consumers will need to be able to specify which
+    // module they want to access.
+    const mod = b.addModule("advent_of_code_2025", .{
+        // The root source file is the "entry point" of this module. Users of
+        // this module will only be able to access public declarations contained
+        // in this file, which means that if you have declarations that you
+        // intend to expose to consumers that were defined in other files part
+        // of this module, you will have to make sure to re-export them from
+        // the root file.
+        .root_source_file = b.path("src/root.zig"),
+        // Later on we'll use this module as the root module of a test executable
+        // which requires us to specify a target.
+        .target = target,
+    });
+
+    // Create executables for each day (day1, day2, etc.) that exists
+    for (1..26) |day_num| {
+        const day = b.fmt("day{d}", .{day_num});
+        const source_path = b.fmt("src/{s}.zig", .{day});
+
+        // Check if the source file exists
+        std.fs.cwd().access(source_path, .{}) catch continue;
+
+        const exe = b.addExecutable(.{
+            .name = day,
+            .root_module = b.createModule(.{
+                .root_source_file = b.path(source_path),
+                .target = target,
+                .optimize = optimize,
+                .imports = &.{
+                    .{ .name = "advent_of_code_2025", .module = mod },
+                },
+            }),
+        });
+
+        b.installArtifact(exe);
+
+        const run_cmd = b.addRunArtifact(exe);
+        run_cmd.step.dependOn(b.getInstallStep());
+
+        if (b.args) |args| {
+            run_cmd.addArgs(args);
+        }
+
+        const run_step = b.step(day, b.fmt("Run {s}", .{day}));
+        run_step.dependOn(&run_cmd.step);
+    }
+
+    // Test step for the shared module
+    const mod_tests = b.addTest(.{
+        .root_module = mod,
+    });
+    const run_mod_tests = b.addRunArtifact(mod_tests);
+
+    const test_step = b.step("test", "Run tests");
+    test_step.dependOn(&run_mod_tests.step);
+}
---
+++ src/day1.zig
@@ -0,0 +1,93 @@
+const std = @import("std");
+
+const totalNumbers = 100;
+
+const RotationDirection = enum(u2) {
+    Left = 0,
+    Right = 1,
+};
+
+const Move = struct { dir: RotationDirection, amount: u16 };
+
+const Dial = struct {
+    position: u16,
+    timesZero: u16,
+
+    fn create(allocator: std.mem.Allocator, start: u16) !*Dial {
+        const dial = try allocator.create(Dial);
+        errdefer allocator.destroy(dial);
+        dial.position = start;
+        dial.timesZero = 0;
+        return dial;
+    }
+
+    fn rotate(self: *Dial, dir: RotationDirection, amount: u16) void {
+        const fixedAmount = amount % totalNumbers;
+        var timesAtZero = amount / totalNumbers;
+        const delta = switch (dir) {
+            .Left => (100 -% fixedAmount),
+            .Right => fixedAmount,
+        };
+        const crossedZero: bool = switch (dir) {
+            .Left => (self.position != 0 and fixedAmount > self.position),
+            .Right => (self.position != 0 and self.position + fixedAmount > totalNumbers),
+        };
+        self.position = (self.position + delta) % totalNumbers;
+        if (self.position == 0) {
+            timesAtZero += 1;
+        }
+        if (crossedZero) {
+            timesAtZero += 1;
+        }
+        self.timesZero += timesAtZero;
+    }
+};
+
+fn parseLine(line: []const u8) !?Move {
+    if (line.len < 2) return null;
+    const dir: RotationDirection = if (line[0] == 'R') .Right else .Left;
+    const amount = try std.fmt.parseInt(u16, line[1..], 10);
+    return Move{ .dir = dir, .amount = amount };
+}
+
+pub fn main() !void {
+    const allocator = std.heap.page_allocator;
+    const dial = try Dial.create(allocator, 50);
+    defer allocator.destroy(dial);
+
+    var file = try std.fs.cwd().openFile("day1input.txt", .{ .mode = .read_only });
+    defer file.close();
+
+    // Create buffered reader
+    var file_buf: [8192]u8 = undefined;
+    var file_reader = file.reader(&file_buf);
+    const reader = &file_reader.interface;
+
+    // Read lines using takeDelimiterExclusive
+    while (reader.takeDelimiterExclusive('\n')) |line| {
+        // Handle Windows line endings
+        var trimmed_line = line;
+        if (line.len > 0 and line[line.len - 1] == '\r') {
+            trimmed_line = line[0 .. line.len - 1];
+        }
+
+        if (trimmed_line.len == 0) continue;
+
+        const parsed = try parseLine(trimmed_line) orelse {
+            std.debug.print("Invalid: {s}\n", .{trimmed_line});
+            continue;
+        };
+        std.debug.print("---------------\n", .{});
+        std.debug.print("dir={s}  amount={d}  pos={d}\n", .{ @tagName(parsed.dir), parsed.amount, dial.position });
+        dial.rotate(parsed.dir, parsed.amount);
+        std.debug.print("new position         pos={d}\n", .{dial.position});
+        std.debug.print("---------------\n", .{});
+        _ = try reader.take(1);
+    } else |err| switch (err) {
+        error.EndOfStream => {},
+        error.StreamTooLong => return err,
+        else => return err,
+    }
+
+    std.debug.print("Final position: {d}, times crossed zero: {d}\n", .{ dial.position, dial.timesZero });
+}
